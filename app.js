81e558c3f549c78d27b6d3b36a7f4a8ea9851672
Ehimare <victorymartial11@gmail.com>
2024-05-31 08:46:34 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,85 @@
+// String Manipulation Functions:
+
+// Reverse a String: Write a function that reverses a given string.
+
+// function reverseString(str){
+//     let reversedString = str.split('').reverse().join('')
+//     console.log(reversedString);
+// }
+
+// reverseString('hell')
+
+// Count Characters: Create a function that counts the number of characters in a string
+
+// function countCharacters(word){
+//     length = word.split('').length
+//     console.log(length);
+// }
+
+// countCharacters('ehimare')
+
+
+// Capitalize Words: Implement a function that capitalizes the first letter of each word in a sentence.
+
+// function capitalizeWord(word){
+//     newWord = word[0].toUpperCase()+ word.slice(1)
+//     console.log(newWord);
+// }   
+
+// capitalizeWord('ehimare')
+
+
+// Array Functions:
+
+// Find Maximum and Minimum: Write functions to find the maximum and minimum values in an array of numbers.
+
+// function maxAndMin(arr){
+    
+// let max = 0
+// let min = 100
+// for(i of arr){
+//    if(i > max){
+//     max = i
+//    }
+//    if(i < min){
+//     min = i
+//    }
+// }
+
+// console.log(max);
+// console.log(object);
+    
+    
+// }
+
+// maxAndMin([1,2,3,4])
+
+
+// Sum of Array: Create a function that calculates the sum of all elements in an array.
+
+// function sumOFArray(arr){
+//     let sum = 0
+
+//     for(i of arr){
+//         sum += i
+//     }
+//     console.log(sum);
+// }
+
+// sumOFArray([1,2,3])
+
+
+//Filter Array: Implement a function that filters out elements from an array based on a given condition.
+
+// function filterArray(arr){
+//     let newArr = [  ]
+//     for(i of arr){
+//         if(i % 2 === 0){
+            
+//             newArr += `${ i } `
+//         }
+//     }
+//     console.log(newArr);
+// }
+
+// filterArray([1,2,3,4,5,6,7,8,9,10])
